535e3fa22cfd0268ff5372edd5a0ed2b1c677dd3
Jasmine Lok <jasminellok@gmail.com>
2020-04-07 00:22:38 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -11,3 +11,8 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 function accessElementInArray(array, index) {
   return array[index]
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
