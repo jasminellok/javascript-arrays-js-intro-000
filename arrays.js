4e7bfcbb8b880d90c94b92376de8072570f0d2e4
Jasmine Lok <jasminellok@gmail.com>
2020-04-07 00:19:24 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,2 +1,14 @@
 chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
-addElementToBeginningOfArray(array, element)
+
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return array.unshift(element)
+}
+
+function accessElementInArray(array, index) {
+  return array[index]
+}
+
